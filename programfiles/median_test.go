7d1ca9a02f76c87964dc2fdbf014cbafe8df18cb
shfana <wambitafana@gmail.com>
2024-05-10 14:05:57 +0300

    Add test for median function

---
+++ programfiles/median_test.go
@@ -0,0 +1,20 @@
+package mathskills
+
+import "testing"
+
+func TestCalcMedian(t *testing.T) {
+	tests := []struct {
+		input    []float64
+		expected float64
+	}{
+		{input: []float64{101, 242, 999, 98, 456}, expected: 242},
+		{input: []float64{100, 200, 500, 300, 400}, expected: 300},
+	}
+
+	for _, test := range tests {
+		output := CalcMedian(test.input)
+		if output != test.expected {
+			t.Errorf("calculateAverage(%v) = %v; Expected %v", test.input, output, test.expected)
+		}
+	}
+}
