284464a225c77967332b5ab2ac88bc7462230b27
shfana <wambitafana@gmail.com>
2024-05-10 14:32:24 +0300

    Add: main function

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+
+	mathskills "mathskills/programfiles"
+)
+
+func main() {
+	data, err := mathskills.OpenFile("data.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	// average , kept as a float for accurate calculations
+	average := mathskills.CalcAverage(data)
+
+	// calculate median and round it safely checking for overflows
+	median, err := mathskills.SafeRound(mathskills.CalcMedian(data))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// calculate variance and keep it as float for accurate calculations
+	variance := mathskills.CalcVariance(data, average)
+
+	// claculate median and round it safely
+	standarddeviation, err := mathskills.SafeRound(mathskills.CalcStdDev(variance))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// round the average safely
+	roundedAverage, err := mathskills.SafeRound(average)
+	if err != nil {
+		fmt.Println(err)
+	}
+	// round the variance safely
+	roundedVariance, err := mathskills.SafeRound(variance)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// Print all values to the nearest integer
+	fmt.Printf("Average: %d\n", roundedAverage)
+	fmt.Printf("Median: %d\n", median)
+	fmt.Printf("Variance: %d\n", roundedVariance)
+	fmt.Printf("Standard Deviation: %d\n", standarddeviation)
+}
